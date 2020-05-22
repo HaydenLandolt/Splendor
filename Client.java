c73582714d70a4a392784bccf245df6acacd3cc0
HaydenLandolt <65735267+HaydenLandolt@users.noreply.github.com>
2020-05-22 11:29:18 -0400

    Client Update

--- Client.java
+++ Client.java
@@ -3,7 +3,7 @@ import java.net.*;
 public class Client
 {
     public static void main(String[] args){
-        String host = "192.168.0.16";
+        String host = "192.168.0.13";
         int port = 9999;
        
         /*
